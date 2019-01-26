9a257fd4455e49a7a1cc83aef9e6ea4f4988fb0c
Corrado Alfano <corrado.alfano10@gmail.com>
2019-01-26 11:01:00 -0800

    change

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
+
 // Our scraping tools
 // Axios is a promised-based http library, similar to jQuery's Ajax method
 // It works on the client and on the server
@@ -143,5 +144,5 @@ app.post("/articles/:id", function(req, res) {
 
 // Start the server
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+  console.log("App running on http://localhost: " + PORT + "!");
 });
